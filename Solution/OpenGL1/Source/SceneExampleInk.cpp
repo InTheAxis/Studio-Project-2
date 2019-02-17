97dd7d48289e982684d4aa67d61cc6c5417338e9
InTheAxis <codeaxex@gmail.com>
2019-02-17 22:19:02 +0800

    fixed sceneExampleink, disabled light when rendering custom fbo basically

--- Solution/OpenGL1/Source/SceneExampleInk.cpp
+++ Solution/OpenGL1/Source/SceneExampleInk.cpp
@@ -29,7 +29,7 @@ void SceneExampleInk::RenderDerived()
 	glBindFramebuffer(GL_FRAMEBUFFER, m_frameBufferID);
 	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	RenderObject(&paintLayer);
+	RenderObject(&paintLayer, false);
 	
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	RenderObject(&floor);
